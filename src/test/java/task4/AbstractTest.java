1b16a96782aee20f157d728cc9558b64a4544b37
Anastasia Elfimova <nastya.elfimova@inbox.ru>
2024-02-10 13:52:13 +0500

    Done

---
+++ src/test/java/task4/AbstractTest.java
@@ -0,0 +1,40 @@
+package task4;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public abstract class AbstractTest {
+    public WebDriver driver;
+
+    @BeforeAll
+    static void initialization() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void init(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.setBrowserVersion("116.0.5845.111");
+        options.addArguments("--incognito");
+//        options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get("https://gb.ru/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    void close() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/task4/UITest.java
@@ -0,0 +1,43 @@
+package task4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.gb.task4.LoginPage;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class UITest extends AbstractTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private LoginPage loginPage;
+    private static final String EMPTYLOGIN = "";
+    private static final String LOGIN = "login";
+    private static final String EMPTYPASSWORD = "";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "login@login.ru";
+
+    @Test
+    void enterWithoutLoginTest() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver, wait);
+        loginPage.authorization(EMPTYLOGIN, PASSWORD);
+        Assertions.assertFalse(driver.findElements(By.xpath("//ul[@id='parsley-id-5']")).isEmpty());
+    }
+
+    @Test
+    void enterWithoutPasswordTest() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver, wait);
+        loginPage.authorization(LOGIN, EMPTYPASSWORD);
+        Assertions.assertFalse(driver.findElements(By.xpath("//ul[@id='parsley-id-7']")).isEmpty());
+
+    }
+
+}
